5ebdd41cbb0834fd00114d75410a74382f327eb2
ashwin970 <ashwinraji97@gmail.com>
2021-02-24 14:49:20 +0530

    enter

--- views/about.js
+++ views/about.js
@@ -2,5 +2,11 @@ new fullpage('#fullpage', {
     sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
     autoScrolling: true,
     navigation: true,
-    navigationPosition: 'right'
+    navigationPosition: 'right',
+    onLeave: (origin, destination, direction)=>{
+      const section = destination.item;
+      const title = section.querySelector("h2");
+      const t1 = new TimelineMax({delay: 0.5});
+      t1.fromTo(title, 0.5, {y:"50", opacity:0}, {y: "0",opacity: 1,ease: Power2.easeInOut});
+    }
   });
